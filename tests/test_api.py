b0a9abcc2c4bf246b114b85b276f8765d1c33e74
Tim van Grieken <tim.van.grieken@signify.com>
2023-03-04 11:09:21 +0000

    small patches

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,18 +8,19 @@ from . import (
     authorisation_response,
     authorisation_response_auth_endpoint_unauthorised,
     test_response_station_data,
-    test_response_stations
+    test_response_stations,
 )
 from incharge.exceptions import AuthorizationError
+
 
 def test_auth_authorized():
     with requests_mock.Mocker() as mock_request:
         mock_request.post(
             "https://businessspecificapimanglobal.azure-api.net/old-authorization/incharge-api/user/token",
             headers=authorisation_response,
-            status_code=HTTPStatus.OK
+            status_code=HTTPStatus.OK,
         )
-        api = InCharge('someuser', 'somepassword')
+        api = InCharge("someuser", "somepassword")
         response = api.authenticate()
         assert response.status_code == HTTPStatus.OK
         assert len(response.content) == 0
@@ -30,48 +31,50 @@ def test_auth_unauthorized():
         mock_request.post(
             "https://businessspecificapimanglobal.azure-api.net/old-authorization/incharge-api/user/token",
             json=authorisation_response_auth_endpoint_unauthorised,
-            status_code=HTTPStatus.UNAUTHORIZED
+            status_code=HTTPStatus.UNAUTHORIZED,
         )
-        api = InCharge('someuser', 'somepassword')
+        api = InCharge("someuser", "somepassword")
         response = api.authenticate()
         assert response.status_code == HTTPStatus.UNAUTHORIZED
-      
 
 
 def test_get_stations():
-     with requests_mock.Mocker() as mock_request:
+    """Test if we can get stations"""
+    with requests_mock.Mocker() as mock_request:
         mock_request.post(
             "https://businessspecificapimanglobal.azure-api.net/old-authorization/incharge-api/user/token",
             headers=authorisation_response,
-            status_code=HTTPStatus.OK
+            status_code=HTTPStatus.OK,
         )
         mock_request.get(
             "https://businessspecificapimanglobal.azure-api.net/station-configuration/pub/stations",
             json=test_response_stations,
             status_code=HTTPStatus.OK,
         )
-        api = InCharge('someuser', 'somepassword')
+        api = InCharge("someuser", "somepassword")
         response = api.get_stations()
         assert response.status_code == HTTPStatus.OK
-        assert response.json()['stations'][0]['name'] == 'station1'
+        assert response.json()["stations"][0]["name"] == "station1"
+
 
 def test_get_stations_unauthorized():
-     with requests_mock.Mocker() as mock_request:
+    with requests_mock.Mocker() as mock_request:
         mock_request.post(
             "https://businessspecificapimanglobal.azure-api.net/old-authorization/incharge-api/user/token",
             json=authorisation_response_auth_endpoint_unauthorised,
-            status_code=HTTPStatus.UNAUTHORIZED
+            status_code=HTTPStatus.UNAUTHORIZED,
         )
-        api = InCharge('someuser', 'somepassword')
+        api = InCharge("someuser", "somepassword")
         response = api.get_stations()
         assert response.status_code == HTTPStatus.UNAUTHORIZED
-        
+
+
 def test_get_station_consumption():
     with requests_mock.Mocker() as mock_request:
         mock_request.post(
             "https://businessspecificapimanglobal.azure-api.net/old-authorization/incharge-api/user/token",
             headers=authorisation_response,
-            status_code=HTTPStatus.OK
+            status_code=HTTPStatus.OK,
         )
         ## Get stations
         mock_request.get(
@@ -85,20 +88,20 @@ def test_get_station_consumption():
             json=test_response_station_data,
             status_code=HTTPStatus.OK,
         )
-        api = InCharge('someuser', 'somepassword')
-        response = api.get_station_consumption(station_name='station1')
-        
+        api = InCharge("someuser", "somepassword")
+        response = api.get_station_consumption(station_name="station1")
+
         assert response.status_code == HTTPStatus.OK
-        assert response.json()[0]['total'] == 1000.00
+        assert response.json()[0]["total"] == 1000.00
+
 
 def test_get_station_consumption_unauthorized():
     with requests_mock.Mocker() as mock_request:
         mock_request.post(
             "https://businessspecificapimanglobal.azure-api.net/old-authorization/incharge-api/user/token",
             json=authorisation_response_auth_endpoint_unauthorised,
-            status_code=HTTPStatus.UNAUTHORIZED
+            status_code=HTTPStatus.UNAUTHORIZED,
         )
-        api = InCharge('someuser', 'somepassword')
-        response = api.get_station_consumption(station_name='station1')
+        api = InCharge("someuser", "somepassword")
+        response = api.get_station_consumption(station_name="station1")
         assert response.status_code == HTTPStatus.UNAUTHORIZED
-        
